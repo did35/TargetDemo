3099f1e547f436159c5eca0989ac9b36a8a72bfb
Didier Delhaisse <did35@yahoo.com>
2026-05-29 11:16:39 +0200

    [Add - ViewModel] DroneVisionViewModel.

---
+++ TargetDemo/ViewModel/DroneVisionViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  DroneVisionViewModel.swift
+//  TargetDemo
+//
+//  Created by Didier Delhaisse on 29/05/2026.
+//
+
+import SwiftUI
+
+@Observable
+class DroneVisionViewModel {
+    var lockState: LockState = .searching
+    var confidence: Double = 0.0
+    
+    func simulateDetection () {
+        withAnimation {
+            confidence = Double.random(in: 0.3...1.0)
+            
+            if confidence > 0.75 {
+                lockState = .locked
+            } else if confidence > 0.45 {
+                lockState = .detected
+            } else {
+                lockState = .searching
+            }
+        }
+    }
+}
