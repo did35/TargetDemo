22683b49030ccc72e633f1dac5038ef7097d5c5d
Didier Delhaisse <did35@yahoo.com>
2026-05-29 09:28:28 +0200

    Initial Commit

---
+++ TargetDemo/TargetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TargetDemoApp.swift
+//  TargetDemo
+//
+//  Created by Didier Delhaisse on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TargetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
